a0adacc5b83c6018e9f6d38dd2de97fd8700f25b
sander1 <sander@plexapp.com>
2012-08-10 02:35:53 +0200

    Misc updates

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,96 +1,137 @@
-
-PLUGIN_TITLE = 'South Park'
-URLS = [
-    ['Denmark',				'http://www.southparkstudios.dk',	'%s/guide/episodes/',	  '%s/guide/episodes/season-%%s'],
-    ['Finland',				'http://www.southparkstudios.fi',	'%s/guide/episodes/',	  '%s/guide/episodes/season-%%s'],
-    ['Germany',				'http://www.southpark.de',	  	'%s/guide/episoden/',	  '%s/guide/episoden/staffel/%%s/'],
-    ['Germany (with original audio)',	'http://www.southpark.de',	  	'%s/guide/episoden/',	  '%s/guide/episoden/staffel/%%s/'],
-    ['The Netherlands',			'http://www.southpark.nl',	  	'%s/guide/episodes/',	  '%s/guide/episodes/season/%%s/'],
-    ['Norway',				'http://www.southparkstudios.no',	'%s/guide/episodes/',	  '%s/guide/episodes/season-%%s'],
-    ['Sweden',				'http://www.southparkstudios.se',	'%s/guide/episodes/',	  '%s/guide/episodes/season-%%s'],
-    ['United States',			'http://www.southparkstudios.com',  	'%s/guide/episodes/',	  '%s/guide/episodes/season-%%s']
-]
-
+NAME = 'South Park'
 THUMB_URL = 'http://southparkstudios-intl.mtvnimages.com/shared/sps/images/south_park/episode_thumbnails/s%se%s_480.jpg'
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
 ICON_PREFS = 'icon-prefs.png'
 
+URLS = {
+	'Denmark': {
+		'base_url':			'http://www.southparkstudios.dk',
+		'guide_url':		'http://www.southparkstudios.dk/guide/episodes/',
+		'seasonguide_url':	'http://www.southparkstudios.dk/guide/episodes/season-%s',
+		'random_url':		'http://www.southparkstudios.dk/full-episodes/random'
+	},
+	'Finland': {
+		'base_url':			'http://www.southparkstudios.fi',
+		'guide_url':		'http://www.southparkstudios.fi/guide/episodes/',
+		'seasonguide_url':	'http://www.southparkstudios.fi/guide/episodes/season-%s',
+		'random_url':		'http://www.southparkstudios.fi/full-episodes/random'
+	},
+	'Germany': {
+		'base_url':			'http://www.southpark.de',
+		'guide_url':		'http://www.southpark.de/guide/episoden/',
+		'seasonguide_url':	'http://www.southpark.de/guide/episoden/staffel/%s/',
+		'random_url':		'http://www.southpark.de/alleEpisoden/random'
+	},
+	'Germany (with original audio)': {
+		'base_url':			'http://www.southpark.de',
+		'guide_url':		'http://www.southpark.de/guide/episoden/',
+		'seasonguide_url':	'http://www.southpark.de/guide/episoden/staffel/%s/',
+		'random_url':		'http://www.southpark.de/alleEpisoden/random'
+	},
+	'The Netherlands': {
+		'base_url':			'http://www.southpark.nl',
+		'guide_url':		'http://www.southpark.nl/guide/episodes/',
+		'seasonguide_url':	'http://www.southpark.nl/guide/episodes/season/%s',
+		'random_url':		'http://www.southpark.nl/episodes/random'
+	},
+	'Norway': {
+		'base_url':			'http://www.southparkstudios.no',
+		'guide_url':		'http://www.southparkstudios.no/guide/episodes/',
+		'seasonguide_url':	'http://www.southparkstudios.no/guide/episodes/season-%s',
+		'random_url':		'http://www.southparkstudios.no/full-episodes/random'
+	},
+	'Sweden': {
+		'base_url':			'http://www.southparkstudios.se',
+		'guide_url':		'http://www.southparkstudios.se/guide/episodes/',
+		'seasonguide_url':	'http://www.southparkstudios.se/guide/episodes/season-%s',
+		'random_url':		'http://www.southparkstudios.se/full-episodes/random'
+	},
+	'United States': {
+		'base_url':			'http://www.southparkstudios.com',
+		'guide_url':		'http://www.southparkstudios.com/guide/episodes/',
+		'seasonguide_url':	'http://www.southparkstudios.com/guide/episodes/season-%s',
+		'random_url':		'http://www.southparkstudios.com/full-episodes/random'
+	}
+}
+
 ####################################################################################################
 def Start():
-    Plugin.AddPrefixHandler('/video/southpark', MainMenu, PLUGIN_TITLE, ICON, ART)
 
-    ObjectContainer.title1 = PLUGIN_TITLE
-    ObjectContainer.art = R(ART)
-    DirectoryObject.thumb = R(ICON)
+	Plugin.AddPrefixHandler('/video/southpark', MainMenu, NAME, ICON, ART)
 
-    #SetVolume()
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	DirectoryObject.thumb = R(ICON)
+
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:14.0) Gecko/20100101 Firefox/14.0.1'
 
 ###################################################################################################
 def MainMenu():
-    oc = ObjectContainer(no_cache=True)
 
-    if Prefs['country'] != "" and Prefs['country'] != None:
-	site = HTML.ElementFromURL(getURLs()[1], errors='ignore')
-	numSeasons = len( site.xpath('//*[contains(@class,"pagination")]//li') )
-	url = GetRandom()
-	oc.add(VideoClipObject(url=url, title=L('RANDOM_TITLE'), thumb=R(ICON)))
-	
-	for season in range(1, numSeasons+1):
-	    title = F("SEASON", str(season))
-	    oc.add(DirectoryObject(key=Callback(Episodes, title=title, season=str(season)), title=title))
+	oc = ObjectContainer(no_cache=True)
 
-    oc.add(PrefsObject(title=L("PREFERENCES"), thumb=R(ICON_PREFS)))
-    return oc
+	if Prefs['country'] != "" and Prefs['country'] is not None:
+		key = Prefs['country']
+
+		random_url = RandomEpisode()
+		oc.add(VideoClipObject(url=random_url, title=L('RANDOM_TITLE'), thumb=R(ICON)))
+
+		guide_url = HTML.ElementFromURL(URLS[key]['guide_url'])
+		num_seasons = len(guide_url.xpath('//*[contains(@class,"pagination")]//li'))
+
+		for season in range(1, num_seasons+1):
+			title = F("SEASON", str(season))
+			oc.add(DirectoryObject(key=Callback(Episodes, title=title, season=str(season)), title=title))
+
+	oc.add(PrefsObject(title=L("PREFERENCES"), thumb=R(ICON_PREFS)))
+
+	return oc
 
 ####################################################################################################
 def Episodes(title, season):
-    oc = ObjectContainer(title2=title)
 
-    availableEpisodes = HTML.ElementFromURL(getURLs()[2] % season, errors='ignore').xpath('//ul[@id="grid_index"]/li[@class="grid_item"]')
+	oc = ObjectContainer(title2=title)
 
-    for episode in availableEpisodes:
-	if episode.xpath('.//a[@class="watch_full_episode"]') and ('/coming-soon-' not in episode.xpath('.//img')[0].get('src')):
-	    epnumber = episode.xpath('.//span[@class="epnumber"]')[0].text.replace('Episode: ', '')
-	    numOnly = epnumber.replace(season, '', 1)
-	    numOnly = numOnly.lstrip('0')
-	    title = episode.xpath('.//span[@class="title eptitle"]')[0].text.strip()
-	    summary = episode.xpath('.//span[@class="epdesc"]')[0].text.strip()
-	    thumb = THUMB_URL % (season.zfill(2), numOnly.zfill(2))
-	    url = episode.xpath('.//a[@class="watch_full_episode"]')[0].get('href')
-	    if url[0:4] != 'http':
-		url = getURLs()[0] + url
+	key = Prefs['country']
+	seasonguide_url = URLS[key]['seasonguide_url']
+	available_episodes = HTML.ElementFromURL(seasonguide_url % season).xpath('//ul[@id="grid_index"]/li[@class="grid_item"]')
 
-	    # Language option for the German website
-	    if Prefs['country'] == 'Germany (with original audio)':
-		url += '?lang=en'
+	for episode in available_episodes:
+		if episode.xpath('.//a[@class="watch_full_episode"]') and ('/coming-soon-' not in episode.xpath('.//img')[0].get('src')):
+			ep_number = episode.xpath('.//span[@class="epnumber"]')[0].text.replace('Episode: ', '')
+			num_only = ep_number.replace(season, '', 1)
+			num_only = num_only.lstrip('0')
+			title = episode.xpath('.//span[@class="title eptitle"]')[0].text.strip()
+			summary = episode.xpath('.//span[@class="epdesc"]')[0].text.strip()
+			thumb = THUMB_URL % (season.zfill(2), num_only.zfill(2))
+			url = episode.xpath('.//a[@class="watch_full_episode"]')[0].get('href')
 
-	    oc.add(EpisodeObject(url=url, title=title, show=PLUGIN_TITLE, season=int(season), index=int(numOnly), summary=summary, thumb=thumb))
-    return oc
+			if url[0:4] != 'http':
+				url = '%s%s' % (URLS[key]['base_url'], url)
 
-####################################################################################################
-def getURLs():
-    for country, base_url, guide_url, seasonguide_url in URLS:
-	if Prefs['country'] == country:
-	    return [base_url, (guide_url % base_url), (seasonguide_url % base_url)]
+		# Language option for the German website
+		if Prefs['country'] == 'Germany (with original audio)':
+			url = '%s%s' % (url, '?lang=en')
 
-###################################################################################################
-def GetRandom():
-    try:
-	content = HTTP.Request(getURLs()[0]+'/full-episodes/random', follow_redirects=False, cacheTime=0).content
-    except Ex.RedirectError, e:
-	if e.headers.has_key('Location'):
-	    redirect_url = e.headers['Location']
-    return redirect_url
+		oc.add(EpisodeObject(url=url, title=title, show=NAME, season=int(season), index=int(num_only), summary=summary, thumb=thumb))
+
+	return oc
 
 ###################################################################################################
-#def SetVolume():
-## Set Flash cookie with volume at max and mute turned off
-#  try:
-#    sol = AMF.SOL('media.mtvnservices.com', 'userPrefs4')
-#    sol.setdefault(u'userPrefs4', {})
-#    sol[u'userPrefs4']['volume'] = 1
-    #sol[u'userPrefs4']['isMute'] = False
-#    sol.save()
-#  except:
-#    Log("Shared Objects folder or Flash cookie 'userPrefs4' does not (yet) exist")
+def RandomEpisode():
+
+	key = Prefs['country']
+	random_url = URLS[key]['random_url']
+
+	try:
+		page = HTTP.Request(random_url, follow_redirects=False).content
+	except Ex.RedirectError, e:
+		if 'Location' in e.headers:
+			url = e.headers['Location']
+
+			if url[0:4] != 'http':
+				url = '%s%s' % (URLS[key]['base_url'], url)
+
+			return url
